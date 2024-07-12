0bc35809e897e4fb132d1d206047d801cbd4ba94
tauseef-khan <tdakhan@hotmail.co.uk>
2024-07-12 11:11:02 +0100

    updated site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dounut-astro.vercel.app",
+  site: "https://dinosdonuts.vercel.app",
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel(),
